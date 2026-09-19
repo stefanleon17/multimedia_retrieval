fc8a359f5cc8c30cb021352d7d0cdf85c7a3e935
Stefan Leon <stefan.leon17@gmail.com>
2026-09-19 18:43:01 +0300

    Added basic object renderer and viewport.

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from obj_viewer import display
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    filename = r"C:\Users\stefa\Desktop\School\Masters\Multimedia Retrieval\m1580.obj"
+    width, height = 900, 700
+    display(filename, width, height)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+       main()
--- obj_viewer.py
+++ obj_viewer.py
@@ -1,5 +1,4 @@
 import sys
-import math
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
@@ -21,24 +20,82 @@ def load_obj(filename):
         elif data[0] == 'f':
             face = []
             for token in data[1:]:
-                t = token.split("/")
-                v = int(t[0]) - 1       # iterating from 0 instead of 1
-                texture = int(t[1])     # Empty, I hope
-                vn = int(t[2]) - 1      # iterating from 0 instead of 1
-                face.append((v, vn))
+                if '/' in token:
+                    t = token.split("/")
+                    v = int(t[0]) - 1           # iterating from 0 instead of 1
+                    # texture = int(t[1]) -1    # Empty, I hope
+                    vn = int(t[2]) - 1          # iterating from 0 instead of 1
+                    face.append((v, vn))
+                else:
+                    v = int(token) - 1          # iterating from 0 instead of 1
+                    face.append((v, None))
             F.append(face)
 
-        return V, VN, F
+    return V, VN, F
+
+def view(width, height, V, VN, F):
+
+    # Perspective & viweport parameters
+    lo = [min(v[i] for v in V) for i in range(3)]
+    hi = [max(v[i] for v in V) for i in range(3)]
+    center = tuple((lo[i] + hi[i]) * 0.5 for i in range(3))
+    radius = max(max(hi[i] - lo[i] for i in range(3)) * 0.5, 1e-6)
+
+    distance = radius * 3.0
+    eye = (center[0] + distance, center[1] + distance, center[2] + distance)
+    up_x, up_y, up_z = 0, 0.5, 0
+
+    fov = 100
+    near = distance * 0.01
+    far = distance + radius * 10
+    aspect = float(width) / float(height)
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    gluLookAt(eye[0], eye[1], eye[2],center[0], center[1], center[2], up_x, up_y, up_z)
+
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(fov, aspect, near, far)
+
+    glViewport(0, 0, width, height)
 
 
-def draw_obj(filename):
+def display(filename, width, height):
     V, VN, F = load_obj(filename)
 
-    # Empty file handling
-    if not V:
-        print('No vertices found')
-        return
+    def draw():
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
+        for face in F:
+            glBegin(GL_POLYGON)
+            for v, vn in face:
+                if vn is not None:
+                    glNormal3f(*VN[vn])
+                glVertex3f(*V[v])
+            glEnd()
 
+        glutSwapBuffers()
+
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(width, height)
+    glutCreateWindow(filename.encode())
+
+    glEnable(GL_DEPTH_TEST)
+    glClearColor(0.16, 0.17, 0.19, 1.0)
+    glColor3f(0.75, 0.76, 0.80)
+    if VN:
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
+        glEnable(GL_NORMALIZE)
+        glEnable(GL_COLOR_MATERIAL)
+        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
+        glLightfv(GL_LIGHT0, GL_POSITION, [10.0, 10.0, 10.0, 1.0])
+
+    view(width, height, V, VN, F)
+
+    glutDisplayFunc(draw)
+    glutMainLoop()
 
 
